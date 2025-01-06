5cc17f82d65c8dd4353d4177b860d32dbfb8ed07
Lpmcardoso <134088447+Lpmcardoso@users.noreply.github.com>
2025-01-06 19:37:24 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # primeiro-projeto-clone-tabnews
+
 Meu primeiro projeto de fato de programação do zero e de fora a fora: um clone do Tab News para o curso.dev do Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Luv u mozin! Se você ta vendo isso e me ama também manda um emoji de coração no zap</h1>; 
+  return (
+    <h1>
+      Luv u mozin! Se você ta vendo isso e me ama também manda um emoji de
+      coração no zap
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("idenetação errada");
 }
 
 export default Home;
